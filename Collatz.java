deff99ef8802ea161e8eec091c45b3046b0c7eb8
yamsteinmetz <yam.steinmetz@post.runi.ac.il>
2024-11-20 21:54:13 +0200

    Home Work 2 yam

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,65 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+		int N = Integer.parseInt(args[0]), num=1, count=1;
+        String mode = args[1]; 
+		if (mode.equals("v")) {
+			System.out.println(num+" ");
+			num= num*3+1;
+			count++;
+			System.out.println(num+" ");
+			while (num!=1) {
+				if(num%2==0) {
+					num=num/2;
+				}else{
+					num= num*3+1;
+				} count++;
+				System.out.println(num+" ");
+				
+			} System.out.println("("+count+")");
+			System.out.println();
+			for (int i = 2; i<=N; i++) {
+				count= 1;
+				num= i;
+				System.out.println(num+" ");
+				while (num !=1) {
+						if (num%2==0) {
+						num=num/2;
+					}else{
+						num=num*3+1;
+					}count++;
+					System.out.println(num+" ");
+				} System.out.println("("+count+")");
+				System.out.println();
+			} System.out.println("Every one of the first "+N+"hailstonebsequences reached 1.");
+			}else if (mode.equals("c")){
+				boolean reached1= false;
+				num=num*3+1;
+				while ((num!=1)) {
+					if(num%2==0) {
+						num=num/2;
+				}else{
+					num= num*3+1;		
+					}
+				}
+				for (int i = 2; i<=N; i++) {
+					num= i;
+					while (num!=1) {
+						if (num%2==0) {
+							num=num/2;
+						}else{
+							num=num*3+1;
+						}
+					}
+				} if (num==1) {
+					reached1=true;
+					
+				}
+				if (reached1) {
+					System.out.println("Every one of the first "+N+" hailstone sequences reached 1.");
+					
+				}
+
+			}
+		}
 	}
-}
--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,30 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
-    }
-}
+        int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
+		// Does the same with the minutes part of the input.
+		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+        int minutesToAdd = Integer.parseInt(args[1]);
+        int totalM,totalH, newM, newH;
+        String printH , printM;
+
+        totalM = (hours*60)+minutes+ minutesToAdd;
+        totalH = totalM/60;
+        newH = totalH %24;
+        newM = totalM - (totalH*60);
+
+        if (newH<10)
+            printH= "0"+newH;
+            else printH= ""+ newH;
+            if (newM<10)
+            printM="0"+newM;
+            else printM = ""+ newM;
+                printM = "" +newM;
+                System.out.println(printH+ ":"+ printM);
+            }
+
+        } 
+
+        
+    
+
+
