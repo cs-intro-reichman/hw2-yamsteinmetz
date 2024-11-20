05fc1729985067d33945c337e5d260594b67a92a
yamsteinmetz <yam.steinmetz@post.runi.ac.il>
2024-11-20 23:09:44 +0200

    Home Work 2 yam

--- CalcPi.java
+++ CalcPi.java
@@ -11,7 +11,7 @@ public class CalcPi {
 			sign *= -1;
 		}
 		System.out.println("pi according to java: "+ Math.PI);
-		System.out.println("pi, approximated: " + (sum*4));
+		System.out.println("pi, approximated:     " + (sum*4));
 	}
 }
 	
